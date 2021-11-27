738e08e3dd5a842a27860f30b69fa99542a5ff7e
seyedJamal <seedjamal2@gmail.com>
2021-11-27 16:29:21 +0330

    seconde-commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Store from './Components/Store';
 
 function App() {
   return (
     <div className="App">
-      
+      <Store/>
     </div>
   );
 }
